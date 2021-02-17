06873a8ff8e27919b79cf14ae4ae554bf26dff70
Jasper LaFortune <jasperlafortune@gmail.com>
2021-02-16 18:50:49 -0700

    completed persons exercise

--- section4/exercises/person.rb
+++ section4/exercises/person.rb
@@ -2,4 +2,25 @@
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+  
+  def introduce
+    puts "Hi, I'm #{self.name} and I'm #{age} years old."
+  end
+
+  def celebrate_birthday
+    self.age += 1
+    puts "Yaaaaay I'm #{age} years old now!"
+  end
+
+end
+
+allison = Person.new("Allison", 26)
+allison.introduce
+allison.celebrate_birthday
