61b1b4b6a768b12b0a5cd28a1a57d98046c2cb3f
Jasper LaFortune <jasperlafortune@gmail.com>
2021-02-16 18:41:47 -0700

    Completed burritos exercise

--- section4/exercises/burrito.rb
+++ section4/exercises/burrito.rb
@@ -6,14 +6,34 @@
 
 class Burrito
   attr_reader :protein, :base, :toppings
+
   def initialize(protein, base, toppings)
     @protein  = protein
     @base     = base
     @toppings = toppings
   end
+
+  def add_topping(topping)
+    @toppings.append(topping)
+  end
+
+  def remove_topping(topping)
+    @toppings.delete(topping)
+  end
+
+  def change_protein(new_protein)
+    @protein = new_protein
+  end
+
 end
 
 dinner = Burrito.new("Beans", "Rice", ["cheese", "salsa", "guacamole"])
 p dinner.protein
 p dinner.base
 p dinner.toppings
+dinner.add_topping("pico")
+p dinner.toppings
+dinner.remove_topping("salsa")
+p dinner.toppings
+dinner.change_protein("steak")
+p dinner.protein
